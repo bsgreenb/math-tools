f4c2f20e751879377b4fd792f4e83e29ba469f43
Ben Greenberg <bsgreenb@gmail.com>
2021-10-23 19:14:46 -0500

    fix version differences and add angle stuff

--- geometry.py
+++ geometry.py
@@ -1,7 +1,7 @@
 from annotation import label
 
 # Plot a line with arrows on both ends
-def plotLine(plt, start, end, txt= None):
+def plot_line(plt, start, end, txt= None):
   plt.annotate(s='', xy=end, xytext=start, arrowprops=dict(arrowstyle='<->'))
   if txt:
     label(plt, txt, end)
