dc62691fc79fc34c11d849cbb2effee69fb4c712
Ben Greenberg <bsgreenb@gmail.com>
2021-10-25 15:33:01 -0500

    label other sides

--- annotation.py
+++ annotation.py
@@ -1,7 +1,7 @@
-def label(plt,text,coords):
+def label(plt,text,coords, offset=(0,10)):
   plt.annotate(text, # this is the text
                  coords, # these are the coordinates to position the label
                  textcoords="offset points", # how to position the text
-                 xytext=(0,10), # distance from text to points (x,y)
+                 xytext=offset, # distance from text to points (x,y)
                  ha='center',
                  fontsize=16) # horizontal alignment can be left, right or center
