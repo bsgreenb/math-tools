0d0265773ed166eb99db97deb8457d7c90dbd402
Ben Greenberg <bsgreenb@gmail.com>
2021-11-21 16:13:47 -0600

    Add alpha and beta angles

--- annotation.py
+++ annotation.py
@@ -1,7 +1,8 @@
-def label(plt,text,coords, offset=(0,10), fontsize=16):
+def label(plt,text,coords, offset=(0,10), fontsize=16, color=None):
   plt.annotate(text, # this is the text
                  coords, # these are the coordinates to position the label
                  textcoords="offset points", # how to position the text
                  xytext=offset, # distance from text to points (x,y)
                  ha='center',
-                 fontsize=fontsize) # horizontal alignment can be left, right or center
+                 fontsize=fontsize,
+                 color=color) # horizontal alignment can be left, right or center
